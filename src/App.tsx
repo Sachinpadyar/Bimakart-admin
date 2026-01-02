69e9da3874ed03641697d8698d53fd6e72080bd9
webtesting22 <111947726+Sachinpadyar@users.noreply.github.com>
2026-01-02 17:07:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // @ts-check
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate} from "react-router-dom";
 import "./App.css"
 import RouteDashboardComponents from "./components/DashboardAllComponents/RouteDashboardComponents/RouteDashboardComponents";
 import { AllModulesImports } from "./components/DashboardAllComponents/AllModulesImports/AllModulesImports";
 
 function App() {
-  const location = useLocation();
+  // const location = useLocation();
   // const isDashboardRoute = location.pathname.toLowerCase().startsWith("/dashboard");
 
   return (
